c5f954ab1835b558bc5a24ef2e35582df52bf2f4
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-02-19 02:43:03 +0530

    Completed User Backend Aeunthication along with hasing password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -45,6 +45,12 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
+// Hashing the password while entering and existed from the database
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
