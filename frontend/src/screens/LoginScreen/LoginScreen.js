466558729c612942f2335b71b6c5d4366723376f
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-02-19 18:44:29 +0530

    Created a Login Form #Bootstrap

--- frontend/src/screens/LoginScreen/LoginScreen.js
+++ frontend/src/screens/LoginScreen/LoginScreen.js
@@ -1,7 +1,45 @@
 import React from "react";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import MainScreen from "../../components/MainScreen";
 
 const LoginScreen = () => {
-  return <div>LOGIN</div>;
+  return (
+    <MainScreen title="LOGIN">
+      <div className="loginContainer">
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              //   value={email}
+              placeholder="Enter email"
+              // onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              //   value={password}
+              placeholder="Password"
+              // onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <Button variant="primary mt-3" type="submit">
+            Submit
+          </Button>
+        </Form>
+        <Row className="py-3">
+          <Col>
+            New Customer ? <Link to="/register">Register Here</Link>
+          </Col>
+        </Row>
+      </div>
+    </MainScreen>
+  );
 };
 
 export default LoginScreen;
