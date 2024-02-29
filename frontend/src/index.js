1fc1441c5805be144d36ba37e769427b2cd0b3a4
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-03-01 00:42:58 +0530

    made some changes for temporary

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,11 @@ import "./index.css";
 import "./bootstrap.min.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
-import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
 
