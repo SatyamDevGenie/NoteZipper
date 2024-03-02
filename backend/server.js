5bb1b9c657cd36b9b79dc23167f663b6cfd027a1
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-03-03 01:27:45 +0530

    note routes basic setup

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,16 @@
 import colors from "colors";
 import express from "express";
 import dotenv from "dotenv";
-import notes from "./data/notes.js";
+// import notes from "./data/notes.js";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
+import noteRoutes from "./routes/noteRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
-// import cors from "cors";
 
 const app = express();
 
 dotenv.config();
 connectDB();
-
-// // Use CORS middleware with options
-
-// const corsOptions = {
-//   origin: "http://localhost:5000", // Replace with your frontend domain
-//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//   credentials: true, // enable credentials (cookies, HTTP authentication)
-//   optionsSuccessStatus: 204, // some legacy browsers (IE11, various SmartTVs) choke on 204
-// };
-
-// app.use(cors(corsOptions));
-
-// Middleware
-// app.use(cors()); // Enable CORS for all routes
 
 app.use(express.json()); // Request for json data
 
@@ -32,16 +18,17 @@ app.get("/", (req, res) => {
   res.send("API is running successfully...");
 });
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
-});
+// app.get("/api/notes", (req, res) => {
+//   res.json(notes);
+// });
 
-app.get("/api/notes/:id", (req, res) => {
-  const note = notes.find((n) => n._id === req.params.id);
-  res.send(note);
-});
+// app.get("/api/notes/:id", (req, res) => {
+//   const note = notes.find((n) => n._id === req.params.id);
+//   res.send(note);
+// });
 
 app.use("/api/users", userRoutes);
+app.use("/api/notes", noteRoutes);
 
 // Middleware for handling the errors
 app.use(notFound);
