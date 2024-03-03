4687c8a5630840f77a5aa5591485bfe39ec726c8
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-03-03 22:11:42 +0530

    Completed Crud Operation for Notes API

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -29,4 +29,44 @@ const getNoteById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getNotes, createNote, getNoteById };
+const updateNote = asyncHandler(async (req, res) => {
+  const { title, content, category } = req.body;
+
+  const note = await Note.findById(req.params.id);
+
+  if (note.user.toString() !== req.user._id.toString()) {
+    res.status(401);
+    throw new Error("You can't perform this action");
+  }
+
+  if (note) {
+    note.title = title;
+    note.content = content;
+    note.category = category;
+
+    const updatedNote = await note.save();
+    res.json(updatedNote);
+  } else {
+    res.status(400);
+    throw new Error("Note not found");
+  }
+});
+
+const deleteNote = asyncHandler(async (req, res) => {
+  const note = await Note.findById(req.params.id);
+
+  if (note.user.toString() !== req.user._id.toString()) {
+    res.status(401);
+    throw new Error("You can't perfor, this action");
+  }
+
+  if (note) {
+    await note.deleteOne();
+    res.json({ message: "Note Removed" });
+  } else {
+    res.status(404);
+    throw new Error("Note not found");
+  }
+});
+
+export { getNotes, createNote, getNoteById, updateNote, deleteNote };
--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -3,6 +3,8 @@ import {
   getNotes,
   createNote,
   getNoteById,
+  updateNote,
+  deleteNote,
 } from "../controllers/noteController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
@@ -10,6 +12,10 @@ const router = express.Router();
 
 router.route("/").get(protect, getNotes);
 router.route("/create").post(protect, createNote);
-router.route("/:id").get(getNoteById);
+router
+  .route("/:id")
+  .get(getNoteById)
+  .put(protect, updateNote)
+  .delete(protect, deleteNote);
 
 export default router;
