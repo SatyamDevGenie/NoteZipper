e018593a13042cf2145f7bea46c4c2e48ef90c22
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-03-09 05:31:32 +0530

    fully completed notes crud operations

---
+++ backend/controllers/noteController.js
@@ -0,0 +1,76 @@
+import { json } from "express";
+import Note from "../models/noteModel.js";
+import asyncHandler from "express-async-handler";
+
+const getNotes = asyncHandler(async (req, res) => {
+  const notes = await Note.find({ user: req.user._id });
+  res.json(notes);
+});
+
+const createNote = asyncHandler(async (req, res) => {
+  const { title, content, category } = req.body;
+
+  if (!title || !content || !category) {
+    res.status(400);
+    throw new Error("Please Fill all the feilds");
+    return;
+  } else {
+    const note = new Note({ user: req.user._id, title, content, category });
+
+    const createdNote = await note.save();
+
+    res.status(201).json(createdNote);
+  }
+});
+
+const getNoteById = asyncHandler(async (req, res) => {
+  const note = await Note.findById(req.params.id);
+
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).json({ message: "Note not Found" });
+  }
+});
+
+const updateNote = asyncHandler(async (req, res) => {
+  const { title, content, category } = req.body;
+
+  const note = await Note.findById(req.params.id);
+
+  if (note.user.toString() !== req.user._id.toString()) {
+    res.status(401);
+    throw new Error("You can't perform this action");
+  }
+
+  if (note) {
+    note.title = title;
+    note.content = content;
+    note.category = category;
+
+    const updatedNote = await note.save();
+    res.json(updatedNote);
+  } else {
+    res.status(404);
+    throw new Error("Note not Found");
+  }
+});
+
+const deleteNote = asyncHandler(async (req, res) => {
+  const note = await Note.findById(req.params.id);
+
+  if (note.user.toString() !== req.user._id.toString()) {
+    res.status(401);
+    throw new Error("You can't perform this action");
+  }
+
+  if (note) {
+    await note.deleteOne();
+    res.json({ message: "Note Removed Successfully" });
+  } else {
+    res.status(404);
+    throw new Error("Note not Found");
+  }
+});
+
+export { getNotes, createNote, getNoteById, updateNote, deleteNote };
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 // import notes from "./data/notes.js";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
+import noteRoutes from "./routes/noteRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 const app = express();
@@ -17,16 +18,17 @@ app.get("/", (req, res) => {
   res.send("API is running successfully...");
 });
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
-});
+// app.get("/api/notes", (req, res) => {
+//   res.json(notes);
+// });
 
-app.get("/api/notes/:id", (req, res) => {
-  const note = notes.find((n) => n._id === req.params.id);
-  res.send(note);
-});
+// app.get("/api/notes/:id", (req, res) => {
+//   const note = notes.find((n) => n._id === req.params.id);
+//   res.send(note);
+// });
 
 app.use("/api/users", userRoutes);
+app.use("/api/notes", noteRoutes);
 
 // Middleware for handling the errors
 app.use(notFound);
