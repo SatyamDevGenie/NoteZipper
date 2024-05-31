03389b73edaba8d86d9de659a0f11b6b0f881b9e
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-06-01 04:47:52 +0530

    make some changes in dj.js and server.js

--- backend/server.js
+++ backend/server.js
@@ -38,4 +38,4 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running on PORT ${PORT}`.yellow));
+app.listen(PORT, console.log(`Server running on PORT ${PORT}`));
