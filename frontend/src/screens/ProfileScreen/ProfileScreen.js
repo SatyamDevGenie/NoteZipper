31f1bd94784e2d65535cfb2a85fabc49e7c0e2cf
Your Name <your.email@example.com>
2024-08-10 06:38:41 +0530

    basic setup done in user profile screen

--- frontend/src/screens/ProfileScreen/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen/ProfileScreen.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import MainScreen from "../../components/MainScreen";
 
 const ProfileScreen = () => {
-  return <div>ProfileScreen</div>;
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const userUpdate = useSelector((state) => state.userUpdate);
+  const { loading, error, success } = userUpdate;
+
+  return (
+    <MainScreen title="EDIT PROFILE">
+      <div>
+        <Row className="profileContainer">
+          <Col md={6}>form</Col>
+          <Col
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            profilPic
+          </Col>
+        </Row>
+      </div>
+    </MainScreen>
+  );
 };
 
 export default ProfileScreen;
