e093537edd0a810ae850a1f390aaff95a2bd9e80
Your Name <your.email@example.com>
2024-08-10 07:04:21 +0530

    added form inputs in profile screen

--- frontend/src/screens/ProfileScreen/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen/ProfileScreen.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Form, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import MainScreen from "../../components/MainScreen";
+import MainScreen from "../../components/MainScreen"; // Ensure correct import
 
 const ProfileScreen = () => {
   const [name, setName] = useState("");
@@ -19,11 +19,87 @@ const ProfileScreen = () => {
   const userUpdate = useSelector((state) => state.userUpdate);
   const { loading, error, success } = userUpdate;
 
+  const postDetails = (pics) => {
+    if (!pics) {
+      return setPicMessage("Please select an Image");
+    }
+    setPicMessage(null);
+
+    if (pics.type === "image/jpeg" || pics.type === "image/png") {
+      const data = new FormData();
+      data.append("file", pics);
+      data.append("upload_preset", "notezipper");
+      data.append("cloud_name", "drhama97q");
+      fetch("https://api.cloudinary.com/v1_1/drhama97q/image/upload", {
+        method: "post",
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setPic(data.url.toString());
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      return setPicMessage("Please select an Image");
+    }
+  };
+
   return (
     <MainScreen title="EDIT PROFILE">
       <div>
         <Row className="profileContainer">
-          <Col md={6}>form</Col>
+          <Col md={6}>
+            <Form>
+              <Form.Group controlId="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group controlId="email">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group controlId="confirmPassword">
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Confirm Password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group controlId="pic">
+                <Form.Label>Change Profile Picture</Form.Label>
+                <Form.Control
+                  type="file"
+                  onChange={(e) => postDetails(e.target.files[0])}
+                />
+              </Form.Group>
+              <Button type="submit" variant="primary">
+                Update
+              </Button>
+            </Form>
+          </Col>
           <Col
             style={{
               display: "flex",
